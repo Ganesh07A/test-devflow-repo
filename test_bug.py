40a0a1ca97f5e70d24f03202f684f21404adbc77
Ganesh Suvarnakar <work.ganeshsuvarnkar@gmail.com>
2026-03-09 17:06:39 +0530

    mathematical functions updated

--- test_bug.py
+++ test_bug.py
@@ -10,13 +10,13 @@ def get_user(user_id):
 
 # division code 
 def divides(a, b):
-    return a / b 
+    return a /  b 
 
 # factorial function
 def factorial_finder(num):
     fact  = 0
     while(num >=0):
-        fact = fact * num
+        fact = fact ** num
         num -=1
 
     return fact
